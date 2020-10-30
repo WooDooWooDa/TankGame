e6591e0676133fda94fd82af7abbd2bc2323cd2b
WooDooWooDa <jeremie-bouchard@hotmail.fr>
2020-10-30 14:54:05 -0400

    Initial commit

---
+++ src/cegepst/engine/RenderingEngine.java
@@ -0,0 +1,71 @@
+package cegepst.engine;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+
+public class RenderingEngine {
+
+    private JFrame frame;
+    private JPanel panel;
+    private BufferedImage bufferedImage;
+
+    private final int windowWidth = 800;
+    private final int windowHeight = 600;
+
+    public RenderingEngine() {
+        initializeFrame();
+        initializePanel();
+    }
+
+    public void start() {
+        frame.setVisible(true);
+    }
+
+    public void stop() {
+        frame.dispose();
+    }
+
+    public Buffer getRenderingBuffer() {
+        bufferedImage = new BufferedImage(windowWidth, windowHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = bufferedImage.createGraphics();
+        graphics.setRenderingHints(getRenderingHints());
+        return new Buffer(graphics);
+    }
+
+    public void renderBufferOnScreen() {
+        Graphics2D graphics2D = (Graphics2D) panel.getGraphics();
+        graphics2D.drawImage(bufferedImage, 0,0, panel);
+        Toolkit.getDefaultToolkit().sync();
+        graphics2D.dispose();
+    }
+
+    public void addInputListener(KeyListener listener) {
+        panel.addKeyListener(listener);
+    }
+
+    private void initializeFrame() {
+        frame = new JFrame();
+        frame.setSize(windowWidth, windowHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);    //empeche la redimension
+        frame.setTitle("Moving Rectangle game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     //programme le bouton X pour quitter le programme
+        frame.setUndecorated(true); //enleve la bar en haut
+    }
+
+    private void initializePanel() {
+        panel = new JPanel();
+        panel.setBackground(Color.BLUE);
+        panel.setFocusable(true);
+        panel.setDoubleBuffered(true);
+        frame.add(panel);
+    }
+
+    private RenderingHints getRenderingHints() {
+        RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        return rh;
+    }
+}
---
+++ src/cegepst/engine/entities/MovableEntity.java
@@ -0,0 +1,44 @@
+package cegepst.engine.entities;
+
+import cegepst.engine.controls.Direction;
+
+public abstract class MovableEntity extends UpdatableEntity {
+
+    private Direction direction = Direction.UP;
+    private int speed = 1;
+
+    public void moveLeft() {
+        move(Direction.LEFT);
+    }
+    public void moveRight() {
+        move(Direction.RIGHT);
+    }
+    public void moveUp() {
+        move(Direction.UP);
+    }
+    public void moveDown() {
+        move(Direction.DOWN);
+    }
+
+    public void move(Direction direction) {
+        this.direction = direction;
+        x += direction.getVelocityX(speed);
+        y += direction.getVelocityY(speed);
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
